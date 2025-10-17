2baa75188176f16210b8a9f2f714392e2bdb1c9c
rrhhsupercode-ctes <rrhh.supercode@gmail.com>
2025-10-17 13:58:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -973,17 +973,17 @@ async function loadStock(filtro = "") {
 
     tr.innerHTML = `
       <td>${id}</td>
-      <td><input type="text" value="${prod.nombre}" maxlength="20" style="width:100%;" data-field="nombre"></td>
+      <td><input type="text" value="${prod.nombre}" maxlength="20" style="width:100%; min-width:110px; box-sizing:border-box;" data-field="nombre"></td>
       <td style="display:flex; align-items:center; gap:4px;">
         <button class="btn-cant" data-action="-">-</button>
-        <input type="number" min="0" max="999" value="${prod.cant}" style="width:56px; text-align:center;" data-field="cant">
+        <input type="number" min="0" max="999" value="${prod.cant}" style="width:56px; min-width:56px; box-sizing:border-box; text-align:center;" data-field="cant">
         <button class="btn-cant" data-action="+">+</button>
       </td>
       <td>${prod.fecha ? formatFecha(prod.fecha) : ""}</td>
       <td style="display:flex; gap:4px; align-items:center;">
-        <input type="text" value="${entero}" style="width:66px; text-align:right;" data-field="precio-entero">
+        <input type="text" value="${entero}" style="width:66px; min-width:66px; box-sizing:border-box; text-align:right;" data-field="precio-entero">
         <span>,</span>
-        <input type="number" min="0" max="99" value="${dec.toString().padStart(2,'0')}" style="width:36px; text-align:center;" data-field="precio-centavos">
+        <input type="number" min="0" max="99" value="${dec.toString().padStart(2,'0')}" style="width:36px; min-width:36px; box-sizing:border-box; text-align:center;" data-field="precio-centavos">
       </td>
       <td><button data-del-id="${id}">❌</button></td>
     `;
